15cbe04c8b115c126867dd5cc7421629ebc3f425
Abel <zxz054321@foxmail.com>
2018-12-26 10:48:21 +0800

    handle object

--- src/AutoGeneratorController.php
+++ src/AutoGeneratorController.php
@@ -5,13 +5,23 @@ namespace Abel\PrivateApi;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Cache;
+use Symfony\Component\HttpFoundation\Response;
 
 class AutoGeneratorController extends Controller
 {
     public function generateRoute(Request $request)
     {
         $privateApi = Cache::get('private-api:route:' . $request->route()->uri);
+        $response   = PrivateApi::app($privateApi['app'])->api($privateApi['api'], $request->all());
 
-        return PrivateApi::app($privateApi['app'])->api($privateApi['api'], $request->all());
+        if ($response instanceof Response) {
+            return $response;
+        }
+
+        if (is_object($response)) {
+            return (array)$response;
+        }
+
+        return $response;
     }
 }
